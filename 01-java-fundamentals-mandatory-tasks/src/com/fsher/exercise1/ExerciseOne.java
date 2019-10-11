7ad958bae1730de81e49ebc9874515ad35c5c744
Filipp <filipp.sher@gmail.com>
2019-10-11 20:54:04 +0300

    Complete first practical exercise, minor cleanup

---
+++ 01-java-fundamentals-mandatory-tasks/src/com/fsher/exercise1/ExerciseOne.java
@@ -0,0 +1,9 @@
+package com.fsher.exercise1;
+
+public class ExerciseOne {
+    public static void main(String[] args) {
+        System.out.println("Out of 1, 5, 2, 0 the lowest is: " + MinFinder.find(1, 5, 2, 0));
+        System.out.println("Out of 5, 5, 5, 5 the lowest is: " + MinFinder.find(5, 5, 5, 5));
+        System.out.println("Out of 100, 2, 3, 10 the lowest is: " + MinFinder.find(100, 2, 3, 10));
+    }
+}
---
+++ 01-java-fundamentals-mandatory-tasks/src/com/fsher/exercise1/MinFinder.java
@@ -0,0 +1,15 @@
+package com.fsher.exercise1;
+
+class MinFinder {
+    public static int find(int a, int b) {
+        return Math.min(a, b);
+    }
+
+    public static int find(int a, int b, int c) {
+        return Math.min(MinFinder.find(a, b), c);
+    }
+
+    public static int find(int a, int b, int c, int d) {
+        return Math.min(MinFinder.find(a, b, c), d);
+    }
+}
