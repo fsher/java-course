381c5fb06049705bd17d7961e3bb00287703fa9d
Filipp <filipp.sher@gmail.com>
2020-06-27 14:48:33 +0300

    Complete second task

---
+++ 07-regexp-mandatory-tasks/src/com/fsher/exercise2/Exercise2.java
@@ -0,0 +1,13 @@
+package com.fsher.exercise2;
+
+public class Exercise2 {
+    private static final String REGEXP_STRING = "     Removing     Extra Spaces    Between Words in    the text      ";
+
+    public static void main(String[] args) {
+        System.out.println(
+                REGEXP_STRING
+                        .replaceAll("(^[\\s]+)|([\\s]+$)", "")
+                        .replaceAll("(\\s{2,})", " ")
+        );
+    }
+}
